2ee151c8c6b181ea5095f89a8376f28137b4a378
Cat Norris <cannenorris@yahoo.com>
2017-11-27 22:11:22 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 
 def my_each (collection)
     i=0
-    while i < collection.lenth
-    yield (collection[i])
+    while i < collection.length
+    yield(collection[i])
     i=i+1
     end
   collection
